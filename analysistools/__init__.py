6502f38c57781982b30c8ccff3561dc23cfbbca7
likeyu <likeyu@megvii.com>
2021-07-29 16:07:56 +0800

    build the setup

---
+++ analysistools/__init__.py
@@ -0,0 +1,9 @@
+from .ModelAnalyticalTool import QModelAnalysis
+from .MultiModelCmp import MultiModelCmp
+from .OneModelAnalyticalTool import OneModelAnalysis
+
+__all__ = [
+    'QModelAnalysis',
+    'MultiModelCmp',
+    'OneModelAnalysis'
+]
